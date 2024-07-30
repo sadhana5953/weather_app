a6bb03c6401957317794e341cf8f170256210087
SADHANA <chaudharysadhana855@gmail.com>
2024-07-30 21:08:42 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+void main()
+{
+  return runApp( Ecommerce_App(),);
+}
+class Ecommerce_App extends StatelessWidget {
+  const Ecommerce_App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
---
+++ lib/utills/colors.dart
@@ -0,0 +1,7 @@
+import 'dart:ui';
+
+Color primaryColor=const Color(0xFF679E38);
+Color secondPrimaryColor=const Color(0xFF8AC24A);
+Color bgColor=const Color(0xFF8AC24A);
+Color container1=const Color(0xFF4CAE50);
+Color container2=const Color(0xFFB1FD59);
